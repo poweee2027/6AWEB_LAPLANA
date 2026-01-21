a97707c136319b62ec2e1d9f0b4aea14fd9e3c5c
Pipowe B. Laplana <pipowepogi@gmail.com>
2026-01-21 15:49:39 +0800

    Add files via upload

---
+++ Midterm Lab Activities/Midterm Lab Activity 5/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Httpclient } from './httpclient';
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('http-client-demo');
+  httpusers: User[] = [];
+
+  constructor(private httpClient: Httpclient) {}
+
+  ngOnInit() {
+    this.httpClient.getUsersRemotely().subscribe((data) => {
+      this.httpusers = data;
+    });
+  }
+}
---
+++ Midterm Lab Activities/Midterm Lab Activity 5/app/httpclient.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Httpclient {
+  constructor(private http: HttpClient) {}
+
+  getUsersRemotely(): Observable<User[]> {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  }
+}
